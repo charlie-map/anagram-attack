02c41719d46a114d54bf24acd5de157ff2f6baa0
Charlie <charjhall777@gmail.com>
2021-11-02 08:54:26 -0400

    initial commit

---
+++ createIndex.js
@@ -0,0 +1,14 @@
+/* use fs with the commands:
+	let dictionary = fs.readFileSync("./smallDictionary.txt");
+	fs.writeFileSync("./myIndex.txt", "A BIG STRING OF STUFF!");
+*/
+const fs = require('fs');
+
+// Think about your components:
+// first you sort each work to
+// get a alphanumerical word, and
+
+// then you sort each word against
+// each other words alphanumerical word
+// Hmm...
+// We wish you well!
